07c363547e3d7f6c12e77f903b5a406d9402bdb6
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-05-17 17:31:21 +0700

    Change application id

---
+++ app/src/main/java/com/binar/ariefaryudisyidik/challengegoldchapter6/ui/HomeFragment.kt
@@ -0,0 +1,98 @@
+package com.binar.ariefaryudisyidik.challengegoldchapter6.ui
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.binar.ariefaryudisyidik.challengegoldchapter6.R
+import com.binar.ariefaryudisyidik.challengegoldchapter6.adapter.MovieAdapter
+import com.binar.ariefaryudisyidik.challengegoldchapter6.data.remote.response.Movie
+import com.binar.ariefaryudisyidik.challengegoldchapter6.databinding.FragmentHomeBinding
+import com.binar.ariefaryudisyidik.challengegoldchapter6.helper.MainViewModel
+import com.binar.ariefaryudisyidik.challengegoldchapter6.helper.UserDataStoreManager
+import com.binar.ariefaryudisyidik.challengegoldchapter6.helper.ViewModelFactory
+import com.binar.ariefaryudisyidik.challengegoldchapter6.viewmodel.MovieViewModel
+import com.binar.ariefaryudisyidik.challengegoldchapter6.viewmodel.UserViewModel
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private val movieViewModel by viewModels<MovieViewModel>()
+    private val userViewModel by viewModels<UserViewModel>()
+    private val args: HomeFragmentArgs by navArgs()
+
+    //    private lateinit var userPreferences: UserPreferences
+    private lateinit var viewModel: MainViewModel
+    private lateinit var pref: UserDataStoreManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        userPreferences = UserPreferences(requireContext())
+//        binding.tvUsername.text = "Welcome, ${userPreferences.getLoggedInUser()}!"
+        pref = UserDataStoreManager(requireContext())
+        viewModel = ViewModelProvider(this, ViewModelFactory(pref))[MainViewModel::class.java]
+
+//        viewModel.getUsername().observe(viewLifecycleOwner) {
+//            binding.tvUsername.text = "Welcome, $it!"
+//        }
+        viewModel.getId().observe(viewLifecycleOwner) {
+            if (it != 0) {
+                val user = userViewModel.getUser(it)
+                binding.tvUsername.text = "Welcome, ${user.username}!"
+            }
+        }
+
+        movieViewModel.movie.observe(viewLifecycleOwner) { setMovieData(it) }
+        movieViewModel.isLoading.observe(viewLifecycleOwner) { showLoading(it) }
+        binding.ibProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setMovieData(movie: List<Movie>) {
+        binding.apply {
+            val movieAdapter = MovieAdapter(movie)
+            rvMovies.setHasFixedSize(true)
+            rvMovies.layoutManager = LinearLayoutManager(activity)
+            rvMovies.adapter = movieAdapter
+            movieAdapter.setOnItemClickCallback(object :
+                MovieAdapter.OnItemClickCallback {
+                override fun onItemClicked(data: Movie) {
+                    findNavController().navigate(
+                        HomeFragmentDirections.actionHomeFragmentToDetailFragment(
+                            data
+                        )
+                    )
+                }
+            })
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
